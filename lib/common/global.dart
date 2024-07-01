505ac8340a18ad3ca8058e6cc86cc1b43b3b1209
qtuan02 <quoctuan200702@gmail.com>
2024-07-01 22:08:53 +0700

    global

--- lib/common/global.dart
+++ lib/common/global.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 const BASE_URL_LOCAL = "http://192.168.1.15:3030";
 // const BASE_URL_LOCAL = "https://doan-be-qh1m.onrender.com";
@@ -15,4 +16,14 @@ void handleShowToast(String message) {
     textColor: Colors.white,
     fontSize: 16.0,
   );
+}
+
+Future<String?> getToken() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getString('token');
+}
+
+Future<void> removeToken() async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.remove('token');
 }
